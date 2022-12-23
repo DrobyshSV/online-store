706bde9b008eb0783af6d0d73afc300017a5c0f2
markuschaaaa <chumakoff03@gmail.com>
2022-12-24 00:55:30 +0300

    feat: add styles to ProductPage and add variables

--- src/components/pages/product/ProductPage.ts
+++ src/components/pages/product/ProductPage.ts
@@ -22,38 +22,52 @@ class ProductPage extends Page {
   
   async createProductContainer(productId: string){
     const dataUrl = 'https://dummyjson.com/products/' + productId;
-    let test = await this.getFetch(dataUrl);
-    console.log(test);
+    let productInfo = await this.getFetch(dataUrl);
     const productContainer = document.createElement('div');
     productContainer.classList.add('product-container');
+
+    const productTitle = productInfo.title;
+    const productDescription = productInfo.description;
+    const productCategory = productInfo.category;
+    const productBrand = productInfo.brand;
+    const productDiscount = productInfo.discountPercentage;
+    const productImages = productInfo.images;
+    const productPrice = productInfo.price;
+    const productRating = productInfo.rating;
+    const productStock = productInfo.stock;
+
     const stucture = `
       <div class = "product__wrap">
         <div class = "product__source">
-          <div class = "product__name"></div>
-          <div class = "product__name"></div>
-          <div class = "product__name"></div>
-          <div class = "product__name"></div>
+          <div class = "product__source source_item"><p>Store<span>/</span></p></div>
+          <div class = "product__source source_item">${productCategory}<span>/</span></div>
+          <div class = "product__source source_item">${productBrand}<span>/</span></div>
+          <div class = "product__source source_item">${productTitle}<span>/</span></div>
         </div>
-        <div class = "product__name"></div>
         <div class = "product__main">
           <div class = "product__galery">
-            <div class = "product__image"><img src="#" alt="product"></img></div>
-            <div class = "product__image"><img src="#" alt="product"></img></div>
-            <div class = "product__image"><img src="#" alt="product"></img></div>
+            <div class = "product__image"><img src="${productImages[1]}" alt="product"></img></div>
+            <div class = "product__image"><img src="${productImages[2]}" alt="product"></img></div>
+            <div class = "product__image"><img src="${productImages[3]}" alt="product"></img></div>
           </div>
-          <div class = "product__img"><img src="#" alt="product"></img></div>
+          
+          <div class = "product__image main_img"><img src="${productImages[4]}" alt="product"></img></div>
+          
           <div class = "product__description">
-            <div class = "product__description item"></div>
-            <div class = "product__description item"></div>
-            <div class = "product__description item"></div>
-            <div class = "product__description item"></div>
-            <div class = "product__description item"></div>
-            <div class = "product__description item"></div>
-          </div>
-          <div class = "product__price">
-            <div class = "product__price item"></div>
-            <div class = "product__price item"></div>
-            <div class = "product__price item"></div>
+            <div class = "product__name">${productTitle}</div>
+            
+            <div class = "product__price">
+              <div class = "product__price elem">${productPrice}$</div>
+              <div class = "product__price elem"><p>add to card</p></div>
+              <div class = "product__price elem"><p>buy now</p></div>
+            </div>
+
+            <div class = "product__description item"><span class = "pre__description">Brand:</span> ${productBrand}</div>
+            <div class = "product__description item"><span class = "pre__description">Category:</span> ${productCategory}</div>
+            <div class = "product__description item"><span class = "pre__description">In stock:</span> ${productStock}</div>
+            <div class = "product__description item"><span class = "pre__description">Description:</span> ${productDescription}</div>
+            <div class = "product__description item"><span class = "pre__description">Discount:</span> ${productDiscount}</div>
+            <div class = "product__description item"><span class = "pre__description">Rating:</span> ${productRating}</div>
           </div>
         </div>
       </div>
@@ -64,7 +78,7 @@ class ProductPage extends Page {
   }
 
   render() {
-    this.createProductContainer('1').then((productContainer) => {
+    this.createProductContainer('12').then((productContainer) => {
       this.container.append(productContainer);
     })
     return this.container;
