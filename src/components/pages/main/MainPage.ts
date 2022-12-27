f47579619a58339adc903e0b312bfcfff0d78213
DrobyshSV <austin@tut.by>
2022-12-27 23:02:31 +0300

    feat: add filter-list category and brand, refactor initial of Filter class

--- src/components/pages/main/MainPage.ts
+++ src/components/pages/main/MainPage.ts
@@ -1,16 +1,21 @@
 import Page from '../../common/Page';
 import AppController from './products/controller/controller';
 import Cards from './products/cards/Cards';
+import Filters from './Filters/Filters';
 
 class MainPage extends Page {
   static TextObject = {};
   private controller: AppController;
   private cards: Cards;
+  private filter: Filters;
+  private filterContainer: HTMLElement;
 
   constructor(id: string) {
     super(id);
     this.controller = new AppController();
     this.cards = new Cards();
+    this.filterContainer = this.getSection('filters__section')
+    this.filter = new Filters()
   }
   inputEvent(e: Event) {
     const target = e.target as HTMLInputElement;
@@ -28,7 +33,8 @@ class MainPage extends Page {
     return section
   }
   render() {
-    this.container.append(this.getSection('filters__section'))
+    this.container.append(this.filterContainer)
+    this.filterContainer.append(this.filter.init())
     this.container.append(this.getSection('cards__section'))
     this.controller.getSources((data) => {
       this.cards.drawProducts(data);
