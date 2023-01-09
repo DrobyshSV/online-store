833b11d182867f58e66fdc1534b29f43d10f9bd2
DrobyshSV <austin@tut.by>
2023-01-09 17:26:45 +0300

    fix: correct veiw of cards according view-mode

--- src/components/pages/main/products/cards/Products.ts
+++ src/components/pages/main/products/cards/Products.ts
@@ -1,41 +1,73 @@
 import './Product.scss';
-import { ProductType } from '../../../../types/types';
+import {ProductType} from '../../../../types/types';
+import CreateHtml from '../../Filters/CreateHtml';
 
-class Products {
+class Products extends CreateHtml {
+  constructor() {
+    super();
+  }
+
   draw(data: Array<ProductType>, viewMode: string) {
-    const productItems =  (document.querySelector('.product-items') as HTMLElement)
+    const productItems = document.querySelector('.product-items') as HTMLElement;
+    productItems.classList.add(`items-${viewMode}`);
     productItems.innerHTML = '';
-    if (viewMode === 'cards') {
-      data.forEach((item, index: number) => {
-        const productCard: HTMLElement = document.createElement('div');
-        productCard.classList.add('product');
-        productCard.setAttribute('key', item.id.toString());
-        const productName: HTMLElement = document.createElement('div');
-        productName.innerHTML = `<span>${item.title}</span>`;
-        const productImg: HTMLImageElement = document.createElement('img');
-        productImg.setAttribute('width', '150px');
-        productImg.setAttribute('height', '150px');
-        productImg.src = item.images[0];
-        const productBrand: HTMLElement = document.createElement('span');
-        productBrand.innerText = item.brand;
-        const productInStock: HTMLElement = document.createElement('span');
-        productInStock.innerText = item.stock.toString();
-        const productDiscont: HTMLElement = document.createElement('span');
-        productDiscont.innerText = item.discountPercentage.toString();
-        const productPriceWrapper: HTMLElement = document.createElement('div');
-        const productPriceTitle: HTMLElement = document.createElement('h3');
-        productPriceTitle.innerText = item.price.toString() + '$';
-        const productAddBtn: HTMLElement = document.createElement('button');
-        productAddBtn.textContent = 'ADD';
-        productCard.append(productName, productImg, productBrand, productInStock, productDiscont, productPriceWrapper);
-        productPriceWrapper.append(productPriceTitle, productAddBtn);
-        productItems.append(productCard);
-      });
-    } else {
-      data.forEach((item, index: number) => {
 
-      });
-    }
+    data.forEach((item, index: number) => {
+      const productCard = this.createElement('div', `product-${viewMode}`);
+      productCard.setAttribute('key', item.id.toString());
+      const productTitle = this.createElement('h3', 'product-title');
+      productTitle.innerText = item.title;
+      const productImg = this.createElement('img', 'card__img') as HTMLImageElement;
+      productImg.setAttribute('width', '150px');
+      productImg.setAttribute('height', '150px');
+      productImg.src = item.images[0];
+      const productCategory = this.createElement('h4', 'product-description');
+      productCategory.innerHTML = `Category: <span>${item.category}</span>`;
+      const productBrand = this.createElement('h4', 'product-description');
+      productBrand.innerHTML = `Brand: <span>${item.brand}</span>`;
+      const productInStock = this.createElement('h4', 'product-description');
+      productInStock.innerHTML = `Stock: <span>${item.stock.toString()}</span>`;
+      const productDiscont = this.createElement('h4', 'product-description');
+      productDiscont.innerHTML = `Discount: <span>${item.discountPercentage.toString()}</span>`;
+      const productPrice = this.createElement('h4', 'product-description');
+      productPrice.innerHTML = `Price: <span>${item.price.toString()}$</span>`;
+      const productButtonsWrapper = this.createElement('div', 'btn-wrapper');
+      const productAddBtn = this.createElement('button', 'add__btn');
+      productAddBtn.textContent = 'ADD';
+      productAddBtn.setAttribute('type', 'button');
+      const productDetailsLink = this.createElement('a', 'details__link') as HTMLLinkElement;
+      productDetailsLink.href = `#product-page/${item.id}`;
+      const productDetails = this.createElement('button', 'details__btn');
+      productDetails.textContent = 'Details';
+      productDetails.setAttribute('type', 'button');
+      productButtonsWrapper.append(productAddBtn, productDetailsLink);
+      productDetailsLink.append(productDetails);
+      if (viewMode === 'cards') {
+        productCard.append(
+          productTitle,
+          productImg,
+          productCategory,
+          productBrand,
+          productInStock,
+          productDiscont,
+          productPrice,
+          productButtonsWrapper
+        );
+
+      } else if (viewMode === 'list') {
+
+        const listImgWrapper = this.createElement('div', 'list-wrapper__img');
+        const listInfoWrapper = this.createElement('h4', 'list-info');
+        const productDescription = this.createElement('p', 'product-description');
+        const productRating = this.createElement('h4', 'product-description');
+        productRating.innerHTML = `Rating: <span>${item.rating.toString()}</span>`;
+        productBrand.innerText = `Description: ${item.description}`;
+        productCard.append(listImgWrapper, listInfoWrapper, productButtonsWrapper);
+        listImgWrapper.append(productImg);
+        listInfoWrapper.append(productTitle, productDescription, productBrand, productPrice, productRating);
+      }
+      productItems.append(productCard);
+    });
 
   }
 }
